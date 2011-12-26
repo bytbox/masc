c74bdbd832f829f7d90eebfd5e1ee9187202e206
Scott Lawrence <bytbox@gmail.com>
2011-12-25 23:47:14 -0500

    Displaying window

--- masc.go
+++ masc.go
@@ -9,5 +9,14 @@ import (
 func main() {
 	flag.Parse()
 
+	gtk.Init(nil)
+
+	window := gtk.Window(gtk.GTK_WINDOW_TOPLEVEL)
+	window.SetPosition(gtk.GTK_WIN_POS_CENTER)
+	window.SetTitle("GTK Go!")
+
+	window.SetSizeRequest(600, 600)
+	window.ShowAll()
+
 	gtk.Main()
 }
