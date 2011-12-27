4bff2d15d64a5706ed9045a62f7bd2bee94fb05a
Scott Lawrence <bytbox@gmail.com>
2011-12-27 02:57:59 -0500

    Adding DialTLS

--- imap.go
+++ imap.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"net"
 	tp "net/textproto"
 	"strings"
@@ -21,6 +22,16 @@ func Dial(addr string) (*Client, error) {
 	return NewClient(conn, host)
 }
 
+func DialTLS(addr string) (*Client, error) {
+	conn, err := tls.Dial("tcp", addr, nil)
+	if err != nil {
+		return nil, err
+	}
+	host := addr[:strings.Index(addr, ":")]
+	return NewClient(conn, host)
+}
+
+// NewClient returns a new Client using an existing connection.
 func NewClient(conn net.Conn, host string) (*Client, error) {
 	text := tp.NewConn(conn)
 	client := &Client{
