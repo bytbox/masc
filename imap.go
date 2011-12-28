58a515d071b7e29372460e486d29527682f4d621
Scott Lawrence <bytbox@gmail.com>
2011-12-27 20:42:06 -0500

    Major refactoring (thread-safety)

--- imap.go
+++ imap.go
@@ -1,10 +1,6 @@
 // Package imap partially implements the Internet Message Access Protocol as
 // defined in RFC 3501.
 //
-// This implementation is thread-safe, but does not take advantage of all the
-// parallelism provided for in the standard. Commands will be performed
-// sequentially.
-//
 // Untagged IMAP responses are parsed into a Mailbox struct, which tracks all
 // currently known information concerning the state of the remote mailbox.
 
@@ -14,29 +10,39 @@ import (
 	"crypto/tls"
 	"errors"
 	"fmt"
+	"io"
 	"net"
 	tp "net/textproto"
 	"strings"
+	"sync"
 )
 
 type Client struct {
+	// The underlying textproto connection may be used to extend the
+	// functionality of this package; however, using Client.Cmd instead is
+	// recommended, as doing so better preserves thread-safety.
 	Text *tp.Conn
+
 	Box  *Mailbox
-	conn net.Conn
-	host string
+
+	tags map[string]chan string
+	tMut *sync.Mutex
 }
 
 // Represents the current known state of the remote mailbox.
 type Mailbox struct {
 	capabilities []string
+	mut          *sync.RWMutex
 }
 
 func (m *Mailbox) Capable(c string) bool {
+	m.mut.RLock()
 	for _, ca := range m.capabilities {
 		if c == ca {
 			return true
 		}
 	}
+	m.mut.RUnlock()
 	return false
 }
 
@@ -47,49 +53,87 @@ func Dial(addr string) (*Client, error) {
 	if err != nil {
 		return nil, err
 	}
-	host := addr[:strings.Index(addr, ":")]
-	return NewClient(conn, host)
+	return NewClient(conn)
 }
 
-// DIalTLS creates a TLS_secured connection to the IMAP server at the given
+// DialTLS creates a TLS_secured connection to the IMAP server at the given
 // address and returns the corresponding Client.
 func DialTLS(addr string) (*Client, error) {
 	conn, err := tls.Dial("tcp", addr, nil)
 	if err != nil {
 		return nil, err
 	}
-	host := addr[:strings.Index(addr, ":")]
-	return NewClient(conn, host)
+	return NewClient(conn)
 }
 
 // NewClient returns a new Client using an existing connection.
-func NewClient(conn net.Conn, host string) (*Client, error) {
+func NewClient(conn net.Conn) (*Client, error) {
 	text := tp.NewConn(conn)
 	client := &Client{
 		Text: text,
 		Box:  &Mailbox{
 			capabilities: []string{},
+			mut: new(sync.RWMutex),
 		},
-		conn: conn,
-		host: host,
+		tags: map[string]chan string{},
+		tMut: new(sync.Mutex),
 	}
+
+	input := make(chan string)
+
+	// Read all input from conn
+	go func() {
+		l, err := text.ReadLine()
+		for err == nil {
+			input <- l
+			l, err = text.ReadLine()
+		}
+		if err == io.EOF {
+			close(input)
+		} else {
+			panic(err)
+		}
+	}()
+
+	// Start the serving goroutine
+	go func() {
+		for {
+			select {
+			case l := <-input:
+				if isUntagged(l) {
+					client.handleUntagged(l[2:])
+					continue
+				}
+				// handle tagged response
+				ps := strings.SplitN(l, " ", 2)
+				tag := ps[0]
+				l = ps[1]
+				client.tMut.Lock()
+				client.tags[tag] <- l
+				client.tMut.Unlock()
+			}
+		}
+	}()
 	return client, nil
 }
 
 func (c *Client) handleUntagged(l string) {
+	c.Box.mut.Lock()
 	switch l[0:strings.Index(l, " ")] {
 	case "CAPABILITY":
 		c.Box.capabilities = strings.Split(l, " ")[1:]
 	default:
 		println(l)
 	}
+	c.Box.mut.Unlock()
 }
 
 func (c *Client) Cmd(format string, args ...interface{}) error {
 	t := c.Text
 	id := t.Next()
+	tag := fmt.Sprintf("x%d", id)
 	t.StartRequest(id)
-	err := t.PrintfLine("x%d %s", id, fmt.Sprintf(format, args...))
+	err := t.PrintfLine("%s %s", tag, fmt.Sprintf(format, args...))
 	if err != nil {
 		return err
 	}
@@ -98,21 +142,12 @@ func (c *Client) Cmd(format string, args ...interface{}) error {
 	t.StartResponse(id)
 	defer t.EndResponse(id)
 
-	l, err := t.ReadLine()
-	if err != nil {
-		return err
-	}
-	for isUntagged(l) {
-		c.handleUntagged(l[2:])
-		l, err = t.ReadLine()
-		if err != nil {
-			return err
-		}
-	}
+	ch := make(chan string)
+	c.tMut.Lock()
+	c.tags[tag] = ch
+	c.tMut.Unlock()
 
-	// TODO make sure the tagged response ends up in the right place. There
-	// really should be a goroutine to handle all of this.
-	l = strings.SplitN(l, " ", 2)[1]
+	l := <-ch
 	if l[0:2] == "OK" {
 		return nil
 	}
