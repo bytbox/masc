2cdf550b42452130d02ce9662a2057501b771a3d
Scott Lawrence <bytbox@gmail.com>
2011-12-28 22:36:25 -0500

    Stub prompt

--- cmd.go
+++ cmd.go
@@ -1,5 +1,42 @@
 package main
 
-func UIMain() {
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sync"
+)
 
+var initInput sync.Once
+var input *bufio.Reader
+
+func prompt() (string, error) {
+	initInput.Do(func (){input = bufio.NewReader(os.Stdin)})
+
+	fmt.Print("masc> ")
+	line, _, err := input.ReadLine()
+	return string(line), err
+}
+
+func tokenize(line string) (toks []string) {
+	return
+}
+
+func UIMain() {
+	// Prompt loop
+	line, err := prompt()
+	for err == nil {
+		// tokenize the line
+		toks := tokenize(line)
+		switch toks {
+		default:
+			fmt.Println("?")
+		}
+		line, err = prompt()
+	}
+	if err != nil && err != io.EOF {
+		panic(err)
+	}
+	fmt.Println("Bye!")
 }
