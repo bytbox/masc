ac57a82b51005f81b6387c4d49d4d7c356eb897b
Scott Lawrence <bytbox@gmail.com>
2011-12-28 21:41:31 -0500

    Initial layout

--- gtk.go
+++ gtk.go
@@ -37,22 +37,37 @@ func Compose() {
 }
 
 func GUIMain() {
-	//gtk.Main()
-}
-
-/*
-	gtk.Init(nil)
-
 	window := gtk.Window(gtk.GTK_WINDOW_TOPLEVEL)
 	window.SetPosition(gtk.GTK_WIN_POS_CENTER)
-	window.SetTitle("GTK Go!")
+	window.SetTitle("Masc")
 
 	window.Connect("destroy", func(ctx interface{}) {
 		gtk.MainQuit()
 	}, nil)
 
-	window.SetSizeRequest(600, 600)
+	outer := gtk.VBox(false, 4)
+
+	// toolbar
+	toolbar := gtk.HBox(false, 1)
+
+	container := gtk.HBox(true, 4)
+
+	// message list
+	lScroll := gtk.ScrolledWindow(nil, nil)
+	lScroll.SetPolicy(gtk.GTK_POLICY_AUTOMATIC, gtk.GTK_POLICY_ALWAYS)
+	container.Add(lScroll)
+
+	// message body
+	bScroll := gtk.ScrolledWindow(nil, nil)
+	bScroll.SetPolicy(gtk.GTK_POLICY_AUTOMATIC, gtk.GTK_POLICY_ALWAYS)
+	container.Add(bScroll)
+
+	outer.PackStart(toolbar, false, false, 0)
+	outer.Add(container)
+	window.Add(outer)
+
+	window.SetSizeRequest(800, 600)
 	window.ShowAll()
 
 	gtk.Main()
-*/
+}
