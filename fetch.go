3851814cd2b963af4c733781ab2476348cb94151
Scott Lawrence <bytbox@gmail.com>
2012-01-09 16:06:28 -0500

    Removing unwanted imap implementation

--- fetch.go
+++ fetch.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"flag"
+	"fmt"
 )
 
 func testFetch() {
 	flag.Parse()
 	uname, passwd := flag.Args()[0], flag.Args()[1]
 	client, err := DialTLS("pop.gmail.com:995")
-	if err != nil { panic(err) }
 	err = client.Auth(uname, passwd)
 	if err != nil { panic(err) }
-	t, err := client.Retr(5)
-	if err != nil { panic(err) }
-	println(t)
+	ms, _, err := client.ListAll()
+	println(len(ms))
+	for _, m := range ms {
+		t, err := client.Retr(m)
+		if err != nil { panic(err) }
+		fmt.Println(t)
+		client.Dele(m)
+	}
 	client.Quit()
 }
