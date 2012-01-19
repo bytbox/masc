14542b1ed5b507dffccb9e63f1fa4101749e5e58
Scott Lawrence <bytbox@gmail.com>
2012-01-19 18:58:23 -0500

    Using curses and 'go build'

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
-include ${GOROOT}/src/Make.inc
-
 TARG = masc
 UI = curses.go notify.go
 GOFILES = masc.go pop3.go send.go fetch.go config.go store.go ${UI}
 
-include ${GOROOT}/src/Make.cmd
+all: ${TARG}
+
+${TARG}: ${GOFILES}
+	go build -o $@
 
 fmt:
 	gofmt -w ${GOFILES}
--- curses.go
+++ curses.go
@@ -1,5 +1,76 @@
 package main
 
-func UIMain() {
+import (
+	t "github.com/nsf/termbox-go"
+	"log"
+)
 
+var (
+	width  int
+	height int
+
+	message string
+)
+
+var chActions = map[rune]func(){}
+
+var keyActions = map[uint16]func(){}
+
+func unknownAction() {
+	panic("unrecognized keystroke")
+}
+
+func act(a func()) {
+	defer func() {
+		if er := recover(); er != nil {
+			e, ok := er.(error)
+			if ok {
+				message = e.Error()
+			} else {
+				message = "unknown error"
+			}
+		}
+	}()
+	a()
+}
+
+func updateSize() {
+	width = t.Width()
+	height = t.Height()
+}
+
+func display() {
+
+}
+
+func UIMain() {
+	t.Init()
+	updateSize()
+	e := t.Event{}
+	for {
+		display()
+		e.Poll()
+		switch e.Type {
+		case t.EVENT_KEY:
+			if e.Ch == 'q' {
+				goto Exit
+			}
+			a, ok := chActions[e.Ch]
+			if !ok {
+				a, ok = keyActions[e.Key]
+			}
+			if ok {
+				act(a)
+			} else {
+				act(unknownAction)
+			}
+		case t.EVENT_RESIZE:
+			updateSize()
+		default:
+			log.Print("warning: unknown event type")
+		}
+	}
+
+Exit:
+	t.Shutdown()
 }
